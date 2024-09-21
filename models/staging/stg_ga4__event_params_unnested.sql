99c9f2951aedf9f5d8830ded9882f4eeb4a0e3be
yhk <yh.kim@outlook.com>
2024-09-22 03:58:38 +1000

    Joining unnested event params

--- models/staging/stg_ga4__event_params_unnested.sql
+++ models/staging/stg_ga4__event_params_unnested.sql
@@ -6,10 +6,10 @@ final as (
     select 
         TO_HEX(MD5(CONCAT(CAST(event_timestamp AS STRING), event_name, user_pseudo_id))) AS event_id,
         ep.key AS param_key,
-        ep.value.string_value,
-        ep.value.int_value,
-        ep.value.float_value,
-        ep.value.double_value
+        ep.value.string_value AS param_value_string,
+        ep.value.int_value AS param_value_int,
+        ep.value.float_value AS param_value_float,
+        ep.value.double_value AS param_value_double
     from events,
     UNNEST(event_params) AS ep
 )
