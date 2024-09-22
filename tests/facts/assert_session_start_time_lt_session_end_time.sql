bb46ec7563caba30e117c6cc3d9ca23e957a4e9e
yhk <yh.kim@outlook.com>
2024-09-22 19:46:32 +1000

    Passed assert_session_start_time_gt_session_end_time

--- tests/facts/assert_session_start_time_lt_session_end_time.sql
+++ tests/facts/assert_session_start_time_lt_session_end_time.sql
@@ -3,5 +3,4 @@ SELECT
     session_start_time,
     session_end_time
 FROM {{ ref('fct_sessions') }}
-WHERE session_start_time >= session_end_time
-LIMIT 10
+WHERE session_start_time > session_end_time
